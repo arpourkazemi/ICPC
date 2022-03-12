44444e827c992574725c4a58e32f33d0c20b0af5
Amirreza Pourkazemi <arpourkazemi@gmail.com>
2022-03-13 02:33:40 +0330

    first configuration and first cpp code!

---
+++ Gereh-first80/cmake-build-debug/CMakeFiles/Gereh_first80.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gereh_first80.dir/1/1.cpp.obj"
+  "Gereh_first80.exe"
+  "Gereh_first80.exe.manifest"
+  "Gereh_first80.pdb"
+  "libGereh_first80.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gereh_first80.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
